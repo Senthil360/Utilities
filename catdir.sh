a81f81c590c1d8f0ee72e24783f243856409f4a5
Senthil Manikandan <senthilmanikandan360@gmail.com>
2018-06-06 18:11:45 +0530

    Fix the alias screw-up

--- catdir.sh
+++ catdir.sh
@@ -1,22 +1,9 @@
-#!/system/etc/bash ##CHANGE this to "#!/system/bin/sh" if your ROM doesn't have "bash" inbuilt
+#!/bin/bash ##CHANGE this to "#!/system/bin/sh" if your ROM doesn't have "bash" inbuilt
 
 ##Senthil360 @XDA-developers.com##
 
 #USAGE :-  sh catdir "Directory location"
 #Example - to view the values of all tunables of interactive governor - - sh /sdcard/catdir "/sys/devices/system/cpu/cpu2/cpufreq/interactive"
-
-android_device=0 #Change the value to 1 for android devices
-
-if [ "$android_device" -eq 1 ]; then
-   ###Your BUSYBOX location
-   BB=/system/xbin/busybox
-   ###Set_alias
-   alias a_cat='$BB cat'
-   alias a_echo='$BB echo'
-else
-   alias a_cat='cat'
-   alias a_echo='echo'
-fi
 
 # colors
 G='\e[01;32m'
@@ -30,9 +17,9 @@ C='\e[01;36m'
 W='\e[01;37m'
 
 ###cpu0 = Your LITTLE cpu
-sca_gov_l=$(a_cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+sca_gov_l=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 ###cpu2 = Your big cpu
-sca_gov_b=$(a_cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor)
+sca_gov_b=$(cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor)
 
 dir=$1
 if [ "$dir" = "vm" ]; then
@@ -46,19 +33,19 @@ fi
 cd $dir
 
 a=1; Col="${C}"
-a_echo -e "${G}=================${N}"
-a_echo -e "${V}$dir${N}"
-a_echo -e "${G}=================${N}"
-a_echo ""
+echo -e "${G}=================${N}"
+echo -e "${V}$dir${N}"
+echo -e "${G}=================${N}"
+echo ""
 
 if [ -e /sdcard/dtmp ]; then
     rm -f /sdcard/dtmp
 fi
 
 for i in $(ls); do
-     a_echo -e "$Col$a-$i${N} : ${Y}$($BB cat $i 2>/dev/null)${N}" 2>/dev/null
-     a_echo "$a-$i-$($BB cat $i 2>/dev/null)" 1>>/sdcard/dtmp
-     a_echo ""
+     echo -e "$Col$a-$i${N} : ${Y}$($BB cat $i 2>/dev/null)${N}" 2>/dev/null
+     echo "$a-$i-$($BB cat $i 2>/dev/null)" 1>>/sdcard/dtmp
+     echo ""
   a=$((a+1))
 done
 
